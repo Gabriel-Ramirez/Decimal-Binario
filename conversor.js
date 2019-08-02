f1409a9d17ec81a5a12a4aa1a3f552390fb0ff22
Gabriel Antonio Ramirez Puentes <gxb170@gmail.com>
2019-08-01 21:25:51 -0600

    Commit inicial para pagina que convierte numeros de Decimal a Binario

---
+++ conversor.js
@@ -0,0 +1,13 @@
+
+//var decimal = 10; 
+function convertir(){
+var decimal=parseInt(document.getElementById("numeroDecimal").value);
+console.log('El numero en decimal es: ')
+console.log(decimal); 
+var binario;
+binario = decimal.toString(2); 
+console.log('El numero en binario es: ');
+console.log(binario);
+document.getElementById('resultadoBinario').innerHTML = binario; 
+}
+window.onload = resultadoBinario; 
