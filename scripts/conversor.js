65744ecccd3ae60aa98b0ed3cbc21230759a7a9c
Gabriel Ramirez <gxb170@gmail.com>
2019-08-21 22:53:45 -0600

    mejoras en el codigo

--- scripts/conversor.js
+++ scripts/conversor.js
@@ -2,6 +2,6 @@ function convertir(){
 let decimal=parseInt(document.getElementById("numeroDecimal").value);
 let binario;
 binario = decimal.toString(2); 
-document.getElementById('resultadoBinario').innerHTML = binario;
+document.getElementById('resultadoBinario').innerHTML = `<h6>Resultado: <h6>${binario}<br><br>`;
 }
 window.onload = resultadoBinario; 
