d8a4a3e51663821845ded49e9e08ae81e6142475
Gabriel Ramirez <gxb170@gmail.com>
2019-08-21 22:44:53 -0600

    11

--- scripts/conversor.js
+++ scripts/conversor.js
@@ -1,14 +1,7 @@
-
-//var decimal = 10; 
 function convertir(){
-var decimal=parseInt(document.getElementById("numeroDecimal").value);
-console.log('El numero en decimal es: ')
-console.log(decimal); 
-var binario;
+let decimal=parseInt(document.getElementById("numeroDecimal").value);
+let binario;
 binario = decimal.toString(2); 
-console.log('El numero en binario es: ');
-console.log(binario);
-
 document.getElementById('resultadoBinario').innerHTML = binario;
 }
 window.onload = resultadoBinario; 
