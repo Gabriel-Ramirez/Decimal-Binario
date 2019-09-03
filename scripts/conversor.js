8dc0dff23559534d459372c468ea3d3d5f224c28
Gabriel Ramirez <gxb170@gmail.com>
2019-09-02 22:08:21 -0600

    Se agrego la opcion de convertir decimal a octal y hexadecimal ademas se agrego footer y un menu

--- scripts/conversor.js
+++ scripts/conversor.js
@@ -1,7 +1,17 @@
-function convertir(){
+$(document).ready(function(){
+    $('#numeroDecimal').keypress(function(e){
+      if(e.keyCode==13)
+      $('#c').click();
+    });
+});
+
+
+
+function convertir(tipo){
 let decimal=parseInt(document.getElementById("numeroDecimal").value);
-let binario;
-binario = decimal.toString(2); 
-document.getElementById('resultadoBinario').innerHTML = `<h6>Resultado: <h6>${binario}<br><br>`;
+let valor;
+valor = decimal.toString(tipo); 
+document.getElementById('resultadoBinario').innerHTML = `<h6>Resultado: <h6>${valor}<br><br>`;
 }
 window.onload = resultadoBinario; 
+
